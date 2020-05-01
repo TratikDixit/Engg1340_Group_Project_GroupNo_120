1d0ba44284b3d983f2aab897730382857320667d
Tratik Dixit <61153077+TratikDixit@users.noreply.github.com>
2020-05-02 01:16:48 +0530

    Create game_controller.h

---
+++ controllers/headers/game_controller.h
@@ -0,0 +1,23 @@
+#ifndef GAME_CONTROLLER
+#define GAME_CONTROLLER
+
+#include "../screens/start_game.h"
+#include "../screens/achievement.h"
+#include "../screens/instruction.h"
+
+
+#include "../controllers/headers/Character_UI.h"
+#include "../controllers/headers/Chest.h"
+#include "../controllers/headers/Enemy.h"
+#include "../controllers/headers/MapController.h"
+#include "../controllers/headers/player_info.h"
+
+using namespace std;
+
+class Game {
+       
+       void Play_Game();  // co-ordinate the calling of all header files
+       
+};
+
+#endif
