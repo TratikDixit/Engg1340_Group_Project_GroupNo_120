4a5d7054d35b2335c1cd0225c87e78b292d0c7dd
Tratik Dixit <61153077+TratikDixit@users.noreply.github.com>
2020-05-03 08:21:09 +0530

    Update achievement.h

--- screens/achievement.h
+++ screens/achievement.h
@@ -4,25 +4,24 @@
 #include <fstream>
 
 using namespace std;
-
-
 void print_achievement_file() 
 { 
-       string line;
-       ifstream myfile ("achievements.txt");
-       if (myfile.is_open())
-      {
-              while ( getline (myfile,line) )
-           {
-            cout << line << '\n';
-            }
-         myfile.close();
+       string x="",file="achievements.txt";
+       ifstream fin;
+      fin.open(file.c_str());
+
+       if (fin.fail()) {
+		cout << "Error in file opening!" 
+			<< endl;
+  		exit(1);
        }
+        while (fin >> x) {
+ 		cout<<x<<endl;
+ 	}
 
-
+	fin.close();     
   return;
 }    
-
 
 void add_achievement(string player_name, int score)
 {
