0668a6413e451708c25e9f6390d07d0430fb67bb
Tratik Dixit <61153077+TratikDixit@users.noreply.github.com>
2020-05-07 20:11:19 +0530

    Update MapController.h

--- controllers/MapController.h
+++ controllers/MapController.h
@@ -8,21 +8,32 @@
 using namespace std;
 
 class MapController {
+   
    vector<string> grid;
    // Stores the enemies in the current map 
+   
    Enemy** enemies; 
+   
    public: 
+   
    void Load_Map(string, Player*);
    // Randomly load enemies in the map 
    void LoadEnemies();
+   
    void Display_Map(Player); 
+   
    void Update_Map(Player&, bool&); 
+   
    void Update_Enemy_Positions();
    // Checks if a given cell is occupied or not
+   
    bool CheckCell(int, int, Position*);
+   
    void console();
 
    int num_enemies;
+
+
 };
 
 #endif
