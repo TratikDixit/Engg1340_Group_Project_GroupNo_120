4c5baa234e3c2e08a2e7d4d2e4068bbb6aeab98b
Tratik Dixit <61153077+TratikDixit@users.noreply.github.com>
2020-05-07 20:06:03 +0530

    Update MapController.cpp

--- controllers/MapController.cpp
+++ controllers/MapController.cpp
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <ctime>
 #include <algorithm>
+
+// standard header files
+
 #include "MapController.h"
 #include "Chest.h"
 #include "game.h"
@@ -16,11 +19,15 @@ using namespace std;
 
 // Helper functions 
 int min(int a, int b) {
+
    return (a > b) ? b : a;
+
 }
 
 int max(int a, int b) {
+
    return (a > b) ? a : b;
+
 }
 
 void MapController::Load_Map(string level_id, Player *player) {
@@ -57,25 +64,32 @@ void MapController::LoadEnemies() {
    for (int i = 0; i < num_enemies; i++) {
       // Create a new enemy character 
       Enemy *enemy = new Enemy; 
+      
       // Choose a random free location for the enemy
       int enemy_x , enemy_y ;
+      
       //srand(time(NULL));
       do {
          enemy_x = rand()%grid.size();
          enemy_y = rand()%grid[0].size();
       }
+      
       while (grid[enemy_x][enemy_y] != '.');
       // Update the position for the enemy 
+      
       (*enemy).SetPosition(enemy_x, enemy_y);
       // Display the enemy 
+      
       grid[enemy_x][enemy_y] = '&';
       // Save the enemy 
+      
       enemies[i] = enemy;
 
    }
 }
 
 bool MapController::CheckCell(int row, int col, Position* playerPosition) {
+   
    // Check if the player is at the cell 
    if (row == playerPosition->x && col == playerPosition->y) {
       return true;
@@ -83,7 +97,9 @@ bool MapController::CheckCell(int row, int col, Position* playerPosition) {
 
    // Check the position of each enemy 
    for (int i = 0; i < num_enemies; i++) {
+      
       Position* enemyPosition = (*enemies[i]).GetPosition();
+   
       if (enemyPosition->x == row && enemyPosition->y == col) {
          return true;
       }
@@ -93,9 +109,11 @@ bool MapController::CheckCell(int row, int col, Position* playerPosition) {
 }
 
 void MapController::Update_Enemy_Positions() {
+   
    for (int i = 0; i < num_enemies; i++) {
       enemies[i]->move_enemy(&grid);
    }
+
 }
 
 void MapController::Display_Map(Player player) {
@@ -105,10 +123,13 @@ void MapController::Display_Map(Player player) {
 
    // Get the position of the player 
    system("cls");
+
    if (!grid.size()) { 
       cout<<"Error: Map is empty"; 
    } else {
+      
       // Defines the size of the region the player can see
+   
       const int CAMERA_SIZE = 10;
       // Get the current position of the player 
       Position* playerPosition = player.GetPosition();
@@ -118,8 +139,10 @@ void MapController::Display_Map(Player player) {
 
       int row_minBound = max(playerPosition->x-CAMERA_SIZE, 0);
       int row_maxBound = min(playerPosition->x+CAMERA_SIZE, grid.size()-1);
+      
       int col_minBound = max(playerPosition->y-CAMERA_SIZE, 0);
       int col_maxBound = min(playerPosition->y+CAMERA_SIZE, grid[0].size()-1);
+      
       for (int i = row_minBound; i < row_maxBound; i++) {
          for (int j  = col_minBound; j < col_maxBound; j++) {  
             // Check if the grid cell is occupied 
@@ -136,43 +159,58 @@ void MapController::Display_Map(Player player) {
 }
 
 void MapController::Update_Map(Player& player, bool &game_over) {
+   
    // Check for collision with enemies 
+   
    for (int i = 0; i < num_enemies; i++) {
+   
       bool isHit = enemies[i]->Enemy_Kill(&player);
+      
       if (isHit) {
          int damage = player.AttackEnemy();
          char ch;
+      
          if (damage != -1) {
             // Reduce the hp for the player 
             bool dead = enemies[i]->damage(damage);                        
             if (dead == true) {
+         
                // Remove the enemy from the map 
                Position* enemyPosition = enemies[i]->GetPosition();
+               
                grid[enemyPosition->x][enemyPosition->y] = '.';
             
                // Create the new array 
+               
                Enemy **newArray = new Enemy*[num_enemies-1];
                // Shift the elements of the dynamic array except current
+               
                copy(enemies, enemies+i, newArray);
                copy(enemies+i+1, enemies+num_enemies, newArray+i);
 
                // Delete the old dynamic array 
+               
                delete[] enemies; 
                // Reduce the number of enemies 
+               
                num_enemies--;
                // Point towards the new array
+               
                enemies = newArray;  
                
                cout<<"You killed the enemy!\n";
                break;
             } else {
                // Get attacked by the enemy
+               
+               
                cout<<"You have been hit! (-20P) \n";
                
                
                if (player.AP > 0) {
                   player.AP = (player.AP > 20) ? player.AP-20 : 0;
-               } else {
+               } 
+               else {
                   player.HP = (player.HP > 20) ? player.HP-20 : 0;
                }
                
@@ -206,10 +244,13 @@ void MapController::Update_Map(Player& player, bool &game_over) {
 
       if (currCell == 'C' || currCell == 'W' || currCell == 'A') {
          // Create and open new chest
+         
          Chest chest(currCell); 
          string item = chest.open();
+         
          cout<<"You got a "<<item<<" (+"<<item.length()*5<<")!"<<endl;
          char ch; 
+         
          cout<<"Press X to equip item... ";
          cin>>ch;
          
@@ -225,6 +266,7 @@ void MapController::Update_Map(Player& player, bool &game_over) {
       } else {
          if (currCell == 'V') {
             // End the game
+         
             game_over = true;
             return ;
          }
@@ -234,6 +276,7 @@ void MapController::Update_Map(Player& player, bool &game_over) {
             
          } else if (currCell == '~') {
             cout<<"You lost 10 HP! ";
+            
             player.HP -= 10;
 
             int checker = rand()%101; 
@@ -244,16 +287,19 @@ void MapController::Update_Map(Player& player, bool &game_over) {
          } else if (currCell == '=' || currCell == '|') {
             // Generate a random number for random events
             int checker = rand()%101;          
+            
             if (checker < 20) {
                // Spider bite with 20% chance
                cout<<"You were bit by a spider (-25HP)! ";
                player.HP = (player.HP > 20) ? player.HP-20 : 0;
-            } else if (checker < 25) {
+            } 
+            else if (checker < 25) {
                // Boulder fall with 5% chance
                cout<<"A boulder fell on you (-50HP, -100AP)! ";
                player.HP = (player.HP > 50) ? player.HP-50 : 0; 
                player.AP = (player.AP > 100) ? player.AP-100 : 0;
-            } else if (checker < 28) {
+            } 
+            else if (checker < 28) {
                // Cursed door with 3% chance 
                cout<<"This door was cursed by Cerberus (-95HP, -500AP, -200 ATTACK)! ";
                player.HP = (player.HP > 95) ? player.HP-95 : 0; 
@@ -289,8 +335,10 @@ void MapController::console() {
    cout<<"beneath the highest mountains in Auphinid. This cold night marks my escape. FEAR.... ME....\n\n";
    cout<<"Press X to continue... ";
    cin>>ch;
+   
    Display_Map(player);
    bool isDead = false;
+   
    while (!game_over) {
       // check if the player can be killed by the enemy
       if (player.HP == 0) {
