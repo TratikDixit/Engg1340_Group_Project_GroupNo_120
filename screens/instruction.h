bf8fb896071a97e69d938a6b6230bf49bee0e09d
Tratik Dixit <61153077+TratikDixit@users.noreply.github.com>
2020-05-07 20:20:30 +0530

    Update instruction.h

--- screens/instruction.h
+++ screens/instruction.h
@@ -1,11 +1,18 @@
 #include <iostream>
+
 using namespace std;
 
 void print_instruction() 
 {
-    string ins = ". represent movable spaces\n"
+
+    string ins = "INSTRUCTION MANUAL"
+        "\n"
+        "\n"
+        "\n"
+     " . represent movable spaces\n"
     "@ represent the starting point and the player pointer\n"
-    "^ represent the end point\n";
+    "# represent the barriers\n";
+    
     // we can add stuff
     
   cout<<ins<<"\n\n\n";
