ccdcf1c7c24cf134f61d06aa21404f16d9f913d2
Tratik Dixit <61153077+TratikDixit@users.noreply.github.com>
2020-05-07 19:12:21 +0530

    Update achievement.h

--- screens/achievement.h
+++ screens/achievement.h
@@ -1,17 +1,21 @@
 
 #include <iostream>
+
 #include "bits/stdc++.h" 
+
 #include <fstream>
+
 using namespace std;
+
 void print_achievement_file() 
 { 
        string x="";
        ifstream fin;
-       fin.open("achievements.txt");
+       fin.open("achievements.txt");   // to call the record of achievements
 
        if (fin.fail()) {
-		cout << "Error in file opening!" << endl;
-  		exit(1);
+		cout << "Error in file opening!" << endl;   
+  		exit(1);                                // precautionary in case the file cannot be opened
        }
         while (getline(fin,x)) 
 	{
@@ -25,10 +29,12 @@ void print_achievement_file()
 void add_achievement(string player_name, int score)
 {
   string result = player_name + "\t"+to_string(score) +"\n" ;
+	
+	// write the achievements to player
     
     ofstream MyFile("achievements.txt", ios::app);
      MyFile <<result;
      MyFile.close(); 
-return;
+     return;
     
 }
