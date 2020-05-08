cf95b1fc1475adf0115c8c11313251d04a1d1c9e
Tratik Dixit <61153077+TratikDixit@users.noreply.github.com>
2020-05-08 11:38:26 +0530

    Update instruction.h

--- screens/instruction.h
+++ screens/instruction.h
@@ -9,9 +9,24 @@ void print_instruction()
         "\n"
         "\n"
         "\n"
-     " . represent movable spaces\n"
-    "@ represent the starting point and the player pointer\n"
-    "# represent the barriers\n";
+         " . represent movable spaces\n"
+    
+        "@ represent the starting point and the player pointer\n"
+    
+        "# represent the barriers\n"
+        
+        "Keys for movement: "
+        
+        "d to move right"
+        
+        "w to move up"
+        
+        "s to move down"
+        
+        "a to move left"
+        
+        "Enjoy the game!"
+        ;
     
     // we can add stuff
     
