603b923ee2b1f9605945d103f380d8b52a07f4f0
Yashwardhann Kumar <yash20008@gmail.com>
2020-05-09 17:23:14 +0530

    Updated item probabilities for game balancing

--- controllers/Chest.cpp
+++ controllers/Chest.cpp
@@ -15,10 +15,10 @@ using namespace std;
 Chest::Chest(char type) {
     if (type == 'C') {
         // Initialize chest with default items
-        items = {"Celestia, Might of the Empty Void", "Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe", "Long Sword","Long Sword","Long Sword", "Crucifix, Harp of Mountains", "Basic Dagger", "Wooden Bow", "Knife","Knife","Knife","Knife","Knife", "Phantomsong, Bauble of Illuminated Dreams", "Sword", "Wooden Spear", "Gloves", "Gandiva, The Invincible Bow of Brahma", "Vijaya, The Divine Bow", "Govardhana, Torch of Eternity", "Blackplate", "Helmet of Odin", "Armor of Endless Souls", "Ivory Chestguard of Lost Glory", "Scaled Tunic of Haunted Sorrow", "Chestpiece", "Helmet", "Robe", "Cloak", "Boots", "Leather Robe", "Leather Helmet", "Steel Breastplate of Condemned Might"};
+        items = {"Celestia, Might of the Dead (Rare)", "Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe", "Long Sword","Long Sword","Long Sword", "Crucifix Battleaxe (Rare)", "Golden Dagger", "Wooden Bow", "Knife","Steel Knife","Steel Knife","Iron Knife","Mighty Knife", "Phantomsong, Bauble of Illuminated Dreams (Rare)", "Sword", "Wooden Spear", "Iron Battle Hammer", "Iron Battle Hammer", "Iron Battle Hammer", "Iron Battle Hammer", "Blackplate", "Helmet of Odin", "Armor of Endless Souls", "Ivory Chestguard of Lost Glory", "Scaled Tunic of Haunted Sorrow", "Chestpiece", "Helmet", "Robe", "Cloak", "Boots", "Leather Robe", "Leather Helmet", "Steel Breastplate of Condemned Might"};
     } else if (type == 'W') {
         // Initialize chest with weapons only
-        items =  {"Celestia, Might of the Empty Void", "Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe", "Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword", "Brutality, Doomblade of Horrid Dreams", "Basic Dagger", "Wooden Bow", "Knife","Knife","Knife","Knife","Knife", "Phantomsong, Bauble of Illuminated Dreams", "Sword", "Wooden Spear","Wooden Spear","Wooden Spear","Wooden Spear","Wooden Spear","Wooden Spear","Wooden Spear", "Gloves", "Gandiva, The Invincible Bow of Brahma", "Vijaya, The Divine Bow"};
+        items =  {"Celestia, Might of the Empty Void", "Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Axe","Iron Battle Axe","Iron Battle Axe","Iron Battle Axe","Iron Axe", "Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword","Long Sword", "Brutality, Doomblade of Horrid Dreams", "Basic Dagger", "Wooden Bow", "Knife","Knife","Knife","Knife","Mighty Battle Axe", "Phantomsong, Bauble of Illuminated Dreams", "Sword", "Wooden Spear","Wooden Spear","Wooden Spear","Wooden Spear","Wooden Spear","Wooden Spear","Wooden Spear", "Iron Battle Hammer", "Iron Battle Hammer", "Iron Battle Hammer"};
     } else if (type == 'A') {
         // Initialize chest with weapons only
         items =  {"Blackplate", "Helmet of Odin", "Armor of Endless Souls", "Ivory Chestguard of Lost Glory", "Scaled Tunic of Haunted Sorrow", "Chestpiece", "Helmet","Helmet","Helmet","Helmet","Helmet","Helmet","Helmet","Helmet","Helmet","Robe","Robe","Robe","Robe","Robe", "Cloak", "Boots", "Leather Robe","Leather Robe","Leather Robe","Leather Robe""Leather Robe","Leather Robe", "Leather Helmet", "Steel Breastplate of Condemned Might"};
@@ -43,7 +43,7 @@ char Chest::getType(string item) {
 
     for (int i = 0; i < items.size(); i++) {
         if (item == items[i]) {
-            if (i < 24) {
+            if (i < 26) {
                 return 'W';
             } else {
                 return 'A';
